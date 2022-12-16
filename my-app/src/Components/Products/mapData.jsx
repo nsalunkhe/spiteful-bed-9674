336dc21b17e31eacd228b63500cf9b75ff7060b5
akshay <akshaykadu24@gmail.com>
2022-12-16 18:28:45 +0530

    filter frontend

--- my-app/src/Components/Products/mapData.jsx
+++ my-app/src/Components/Products/mapData.jsx
@@ -1,24 +1,53 @@
-import { Box, Heading, HStack, Img, SimpleGrid, Spacer } from '@chakra-ui/react'
+import { Box, Checkbox, CheckboxGroup, CheckboxIcon, Heading, HStack, Img, Select, SimpleGrid, Spacer, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const MapData = ({Products}) => {
   console.log(Products)
   return (
     <Box>
-      <SimpleGrid columns={[1, 2, 3, 4]} spacing={8}>
+      <Box display="flex" >
+        <Box width="20%">
+          <Box  position="Fixed"> 
+            <Heading  paddingLeft="-50px" marginLeft="0px" size='md'>Gender</Heading>
+
+            <Box display="flex" flexDirection="column" justifyContent="flex-start">
+      
+              <Checkbox pl="6" size='md' colorScheme='green' >Mens</Checkbox>
+              <Checkbox pl="6" size='md' colorScheme='green' >Womens</Checkbox>
+              <Checkbox pl="6" size='md' colorScheme='green' >Boys</Checkbox>
+              <Checkbox pl="6" size='md' colorScheme='green' >Girls</Checkbox>
+                
+            </Box>
+            <Heading pl={-10} size='md'>Size</Heading>
+            <Box display="flex" flexDirection="column" justifyContent="flex-start">
+            
+              <Checkbox pl="6" size='md' colorScheme='green' >S</Checkbox>
+              <Checkbox pl="6" size='md' colorScheme='green' >M</Checkbox>
+              <Checkbox pl="6" size='md' colorScheme='green' >L</Checkbox>
+              <Checkbox pl="6" size='md' colorScheme='green' >XL</Checkbox>
+              <Checkbox pl="6" size='md' colorScheme='green' >XXL</Checkbox>
+
+            </Box>
+
+          </Box>
+        </Box>
+      <SimpleGrid columns={[1, 2, 3, 4]} spacing={8} width="100%">
         {
         Products.map((prod)=>{
-            return <Box key={prod.id}>
+            return <Box key={prod.id} style={{fontFamily:"'Noto Sans', sans-serif" ,boxShadow: " rgba(0, 0, 0, 0.16) 0px 1px 4px", paddingBottom:"10px"}}>
                 <Img src={prod.images[0]} alt="product image" />
-                <Box width="95%" margin="auto"  >
-                  <Heading size="sm">{prod.title}</Heading>
-                  <p><span style={{color:'black',fontWeight:'bold'}}>Color:</span> {prod.actual_color}</p>
-                  <p><span style={{color:'black',fontWeight:'bold'}}>Brand:</span> {prod.brand}</p>
-                  <p><span style={{color:'black',fontWeight:'bold'}}>Size:</span> {prod.size}</p>
+                <Box width="95%" margin="auto">
+                  <Box>
+                    <Heading size="sm">{prod.title}</Heading>
+                    <p><span style={{color:'black',fontWeight:'bold'}}>Color:</span> {prod.actual_color}</p>
+                    <p><span style={{color:'black',fontWeight:'bold'}}>Brand:</span> {prod.brand}</p>
+                    <p><span style={{color:'black',fontWeight:'bold'}}>Size:</span> {prod.size}</p>
+                  </Box>
+                 
                   <HStack >
-                    <p style={{ border:"2px solid black" ,padding:"5px 7px 5px 7px" ,}}><span style={{color:'black',fontWeight:'bold'}}>MRP:</span> ₹{prod.variant_mrp}</p>
+                    <p style={{backgroundColor:"#f22f72", textDecoration:"line-through" , color:"whitesmoke" ,padding:"5px 7px 5px 7px" ,}}><span style={{fontWeight:'bold'}}>MRP:</span> ₹{prod.variant_mrp}</p>
                     <Spacer/>
-                    <p style={{backgroundColor:'green', border:"1px solid black" ,padding:"5px 7px 5px 7px" ,color:'white'}}><span style={{fontWeight:'bold'}}>Offer Price:</span> ₹{prod.variant_price}</p>
+                    <p style={{backgroundColor:"#1caf21" ,padding:"5px 7px 5px 7px" ,color:"whitesmoke"}}><span style={{fontWeight:'bold'}}>Offer Price:</span> ₹{prod.variant_price}</p>
                   </HStack>
                 </Box>
 
@@ -26,6 +55,7 @@ const MapData = ({Products}) => {
         })
         }
       </SimpleGrid>
+      </Box>
     </Box>
   )
 }
--- my-app/src/Pages/Products.jsx
+++ my-app/src/Pages/Products.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import MapData from '../Components/Products/mapData'
 import { getProductsData } from '../Components/Products/redux/products.action'
 
 const Products = () => {
+  const [data,setdata] = useState([])
   const Products = useSelector((store)=>(store.productsManager.data))
   const dispatch = useDispatch()
   console.log(Products)
+
+  // const filtMen =()=>{
+  //   setData(Products.map((prod)=>prod.ideal_for==="Men"))
+  // } 
+  // console.log(filtMen)
   useEffect(()=>{
     getProductsData(dispatch)
   },[])
